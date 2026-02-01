aa1dbe0562e59b74bebb6745827b19ffedc499e4
smitbahadure1 <bahaduresmit08@gmail.com>
2026-02-02 00:09:32 +0530

    Disable new architecture and remove Paper provider to fix crash

--- App.js
+++ App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { Provider as PaperProvider } from 'react-native-paper';
 import AppNavigator from './src/navigation/AppNavigator';
 
 export default function App() {
     return (
         <SafeAreaProvider>
-            <PaperProvider>
-                <AppNavigator />
-            </PaperProvider>
+            <AppNavigator />
         </SafeAreaProvider>
     );
 }
